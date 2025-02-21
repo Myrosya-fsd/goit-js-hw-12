01ca62174e9eaf99af119225759eac4d56ef7963
myroslava <murosja80@gmail.com>
2025-02-21 12:07:03 +0200

    hw_12

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,20 +3,17 @@ import axios from 'axios';
 const API_KEY = '48845906-64ae9219885c1d872e65b26b8';
 const BASE_URL = 'https://pixabay.com/api/';
 
-export function searchImages(query) {
+export async function searchImages(query, page = 1) {
   const params = {
     key: API_KEY,
     q: query,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: 'true',
+    per_page: 15,
+    page,
   };
-  return axios
-    .get(BASE_URL, { params })
-    .then(response => {
-      return response.data.hits;
-    })
-    .catch(error => {
-      console.log(error);
-    });
+
+  const response = await axios.get(BASE_URL, { params });
+  return response.data;
 }
--- src/main.js
+++ src/main.js
@@ -1,39 +1,85 @@
 import { searchImages } from './js/pixabay-api.js';
-import { showErrorMessage, renderAllCards } from './js/render-functions.js';
+import {
+  showErrorMessage,
+  renderAllCards,
+  renderAppendCards,
+} from './js/render-functions.js';
 
 const refs = {
   form: document.querySelector('.form'),
-  input: document.querySelector('.form-input'),
-  button: document.querySelector('.form-button'),
   gallery: document.querySelector('.gallery-list'),
   loader: document.querySelector('.loader'),
+  more: document.querySelector('.load-more'),
 };
 
-refs.form.addEventListener('submit', handleFormSubmit);
+refs.form.addEventListener('submit', handleFormSubmitAsync);
+refs.more.addEventListener('click', handleLoadMoreClick);
 refs.loader.style.display = 'none';
 
-function handleFormSubmit(event) {
+let searchQuery = '';
+let page = 1;
+
+async function handleFormSubmitAsync(event) {
   event.preventDefault();
   const value = event.currentTarget.elements.search.value.trim();
   if (!value) {
     showErrorMessage('Enter some value');
     return;
   }
+  searchQuery = value;
+  page = 1;
   renderAllCards(refs.gallery, []);
   refs.loader.style.display = 'block';
-  searchImages(value)
-    .then(data => {
-      refs.loader.style.display = 'none';
-      if (data.length === 0) {
+  try {
+    const data = await searchImages(value);
+    if (data.hits.length === 0) {
+      showErrorMessage(
+        'Sorry, there are no images matching your search query. Please try again!'
+      );
+    } else {
+      renderAllCards(refs.gallery, data.hits);
+      if (data.hits.length < 15) {
         showErrorMessage(
-          'Sorry, there are no images matching your search query. Please try again!'
+          "We're sorry, but you've reached the end of search results."
         );
       } else {
-        renderAllCards(refs.gallery, data);
+        refs.more.style.display = 'block';
       }
-    })
-    .catch(error => {
-      refs.loader.style.display = 'none';
-      showErrorMessage('Sorry, something went wrong. Please try again!');
+    }
+  } catch (error) {
+    showErrorMessage('Sorry, something went wrong. Please try again!');
+  } finally {
+    refs.loader.style.display = 'none';
+  }
+}
+
+async function handleLoadMoreClick() {
+  page += 1;
+  refs.loader.style.display = 'block';
+  refs.more.style.display = 'none';
+  try {
+    const data = await searchImages(searchQuery, page);
+    if (data.hits.length < 15 || page * 15 > data.totalHits) {
+      showErrorMessage(
+        "We're sorry, but you've reached the end of search results."
+      );
+    } else {
+      refs.more.style.display = 'block';
+    }
+    renderAppendCards(refs.gallery, data.hits);
+    window.scrollBy({
+      top: getCardHeight(),
+      behavior: 'smooth',
     });
+  } catch (error) {
+    showErrorMessage('Sorry, something went wrong. Please try again!');
+  } finally {
+    refs.loader.style.display = 'none';
+  }
+}
+
+function getCardHeight() {
+  const card = document.querySelector('.gallery-item:nth-child(1)');
+  const rect = card.getBoundingClientRect();
+  return rect.height * 2;
 }
